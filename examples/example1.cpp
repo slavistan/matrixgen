33c3e5ac1914a7a029d27d65e80e951a2591eb40
Stanislaw Hüll <Stanislaw.Huell@js-soft.com>
2019-06-23 17:26:02 +0200

    Add comments

--- examples/example1.cpp
+++ examples/example1.cpp
@@ -10,8 +10,9 @@ int main()
       {1, 0, 0}, {-1, 0,0}, {0, 1, 0}, {0, -1, 0}, {0, 0, 1}, {0, 0, -1}, {0, 0, 0}
   }};
 
-  // Generate the matrix from the grid's dimensions and the stencil.
-  const auto mat = matrixgen::adjmat1(3, 3, 3, stencil);
+  // Generate the matrix from the grid's dimensions and the stencil. We use a 3x3x1 grid to keep the output small
+  // enough for the poor terminal.
+  const auto mat = matrixgen::adjmat1(3, 3, 1, stencil);
 
   // Print the matrix. Converting to a dense matrix beforehand make the output look cleaner.
   std::cout << std::endl << Eigen::MatrixXd(mat) << std::endl;
