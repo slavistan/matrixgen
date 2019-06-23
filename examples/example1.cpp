c87134b8efe9386ee8f2f3528de9253c17867924
Stanislaw Hüll <Stanislaw.Huell@js-soft.com>
2019-06-23 17:24:18 +0200

    Add comments to example

--- examples/example1.cpp
+++ examples/example1.cpp
@@ -7,10 +7,14 @@ auto lambda = [](int a) { return a; };
 
 int main()
 {
+  // Choose a stencil. We use a symmetric 7p-stencil for this example. 
   const auto stencil = std::array<matrixgen::DiscretePoint3d, 7>{{
       {1, 0, 0}, {-1, 0,0}, {0, 1, 0}, {0, -1, 0}, {0, 0, 1}, {0, 0, -1}, {0, 0, 0}
   }};
 
+  // Generate the matrix from the grid's dimensions and the stencil.
   const auto mat = matrixgen::adjmat1(3, 3, 3, stencil);
+
+  // Print the matrix. Converting to a dense matrix beforehand make the output look cleaner.
   std::cout << std::endl << Eigen::MatrixXd(mat) << std::endl;
 }
