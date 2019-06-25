58e2e170472b32eb27d132409bb725e4ad6a28c7
Stanislaw Hüll <Stanislaw.Huell@js-soft.com>
2019-06-25 19:34:09 +0200

    Add a TODO:

--- test/unit-tests.cpp
+++ test/unit-tests.cpp
@@ -1,13 +1,11 @@
-#include <iostream>
 #include <array>
+#include <iostream>
 
 #include <matrixgen/adjacency-matrix.hpp>
-#include <Eigen/Dense>
 
-auto lambda = [](int a) { return a; };
+#include <Eigen/Dense>
 
 int main()
 {
-  const auto stencil = std::array<std::array<int, 3>, 7>{{
-      {1, 0, 0}, {-1, 0,0}, {0, 1, 0}, {0, -1, 0}, {0, 0, 1}, {0, 0, -1}, {0, 0, 0}}};
+  //TODO: Setup Catch2 for unit-testing using CMakeShift & spack.
 }
