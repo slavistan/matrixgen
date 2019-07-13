0b14dd2fb20eff84a0f1f343429cdfa2bf3328cb
Stanislaw Hüll <Stanislaw.Huell@js-soft.com>
2019-07-13 10:36:44 +0200

    Add comment:

--- include/matrixgen/stencil.hpp
+++ include/matrixgen/stencil.hpp
@@ -8,7 +8,7 @@ template <std::size_t SIZE, typename Scalar_t = int>
 const std::array<std::array<Scalar_t, 3>, SIZE> STENCIL;
 
 /*
- *  7p - STENCIL
+ * Symmetric 7p-stencil
  */
 template <typename Scalar_t>
 const std::array<std::array<Scalar_t, 3>, 7> STENCIL<7, Scalar_t> =
@@ -17,7 +17,7 @@ const std::array<std::array<Scalar_t, 3>, 7> STENCIL<7, Scalar_t> =
     {  1,  0,  0}, {  0,   1,  0}, {  0,  0,  1}}};
 
 /*
- *  19p - STENCIL
+ * Symmetric 19p-stencil
  */
 template <typename Scalar_t>
 const std::array<std::array<Scalar_t, 3>, 19> STENCIL<19, Scalar_t> =
@@ -28,7 +28,7 @@ const std::array<std::array<Scalar_t, 3>, 19> STENCIL<19, Scalar_t> =
     { -1,  0, -1}, { -1,  0,  1}, {  1,  0, -1}, {  1,  0,  1},
     {  0, -1, -1}, {  0, -1,  1}, {  0,  1, -1}, {  0,  1,  1}}};
 /*
- *  27p - STENCIL
+ * Symmetric 27p-stencil
  */
 template <typename Scalar_t>
 const std::array<std::array<Scalar_t, 3>, 27> STENCIL<27, Scalar_t> =
