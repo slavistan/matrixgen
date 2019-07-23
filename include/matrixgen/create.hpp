a3cc8b2382619d7704ff77674b6551d0c186ae93
Stanislaw Hüll <Stanislaw.Huell@js-soft.com>
2019-07-23 13:24:38 +0200

    Dense matrices may be created

--- include/matrixgen/create.hpp
+++ include/matrixgen/create.hpp
@@ -5,21 +5,95 @@
 #pragma once
 
 #include <Eigen/Sparse>
+#include <Eigen/Dense>
 #include <gsl/gsl_assert>
 
 #include <initializer_list>
-namespace matrixgen {
+
+namespace matrixgen::implementation {
 
 template <
+  typename OutMatrix_t
+    >
+struct Create {};
+
+/**
+ * \brief Create Eigen::Matrix objects
+ *
+ * Usage:
+ */
+template <
+  typename EigenScalar_t,
+  int32_t ROWS,
+  int32_t COLS,
+  int32_t OPTIONS,
+  int32_t MAXROWS,
+  int32_t MAXCOLS
+    >
+struct Create<Eigen::Matrix<EigenScalar_t, ROWS, COLS, OPTIONS, MAXROWS, MAXCOLS>> {
+
+  using Matrix_t = Eigen::Matrix<EigenScalar_t, ROWS, COLS, OPTIONS, MAXROWS, MAXCOLS>;
+
+  static
+  Matrix_t
+  create(uint32_t numRows, uint32_t numCols, const std::vector<EigenScalar_t>& list) {
+
+    auto denseMat = Matrix_t(numRows, numCols);
+    for(uint32_t row = 0; row < numRows; ++row) {
+      for(uint32_t col = 0; col < numCols; ++col) {
+         denseMat(row, col) = list[row * numCols + col];
+      }
+    }
+    return denseMat;
+  }
+};
+//  
+//template <
+//  typename EigenScalar_t,
+//  int EigenAlignment,
+//  typename EigenIndex_t
+//    >
+//struct Create<Eigen::SparseMatrix<EigenScalar_t, EigenAlignment, EigenIndex_t>> {
+//
+//  using Matrix_t = Eigen::SparseMatrix<EigenScalar_t, Eigen::RowMajor, EigenIndex_t>;
+//
+//  Matrix_t
+//  create(uint32_t numRows, uint32_t numCols, std::initializer_list<EigenScalar_t> list) {
+//
+//    auto denseMat = Eigen::Matrix<EigenScalar_t, Eigen::Dynamic, Eigen::Dynamic>(numRows, numCols);
+//    for(uint32_t row = 0; row < numRows; ++row) {
+//      for(uint32_t col = 0; col < numCols; ++col) {
+//         
+//      }
+//    }
+//  }
+
+} // namespace matrixgen::implementation
+
+namespace matrixgen {
+
+/**
+ * Create matrices using a simple syntax:
+ *
+ * auto myMatrix = matrixgen::create<MyMatrixType>(
+ *     numRows, NumCols,
+ *     { 3.14, 2.71, .. dense list of all elements .., 0.71 });
+ *
+ * This function is a simple dispatcher for the chosen matrix type. See the
+ * specialization for details.
+ */
+template <
   typename OutMatrix_t,
-  typename ListType_t = double
+  typename Scalar_t = double
     >
 OutMatrix_t create(
     uint32_t numRows,
     uint32_t numCols,
-    std::initializer_list<ListType_t> list) {
+    const std::vector<Scalar_t>& list) {
 
+  Expects( numRows * numCols == list.size() );
 
+  return implementation::Create<OutMatrix_t>::create(numRows, numCols, list);
 }
 
 }
--- unittests/unittests.cpp
+++ unittests/unittests.cpp
@@ -3,6 +3,7 @@
  * \author Stanislaw Hüll
  * \brief Unit tests
  */
+
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 
@@ -10,6 +11,20 @@
 
 #include <Eigen/Core>
 
-TEST_CASE("") {
+TEST_CASE("Creation of matrices works") {
 
+  using DenseMatrix_t = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>;
+  auto myMatrix = matrixgen::create<DenseMatrix_t>(3, 2,
+      { 3.14,   0,
+           0, 1.1,
+         9.2,   0 });
+
+  std::cout << myMatrix << std::endl;
+  using DenseMatrix2_t = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
+  auto myMatrix2 = matrixgen::create<DenseMatrix2_t>(3, 2,
+      { 3.14,   0,
+           0, 1.1,
+         9.2,   0 });
+  std::cout << myMatrix2 << std::endl;
+  (void)myMatrix;
 }
