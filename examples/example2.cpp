c6bc795e91848efb3a102cbf44db0a45a1e45620
Stanislaw Hüll <Stanislaw.Huell@js-soft.com>
2019-08-17 14:33:48 +0200

    Cleanup example 2.

--- examples/example2.cpp
+++ examples/example2.cpp
@@ -1,3 +1,7 @@
+/**
+ * Example displaying the generation of adjacency matrices using a compact
+ * syntax. Equivalent to example 1.
+ */
 #include <array>
 #include <iostream>
 
@@ -5,29 +9,11 @@
 
 int main()
 {
-  // In this example we choose a weight-function which returns -1 for the diagonal elements of the matrix. The elements
-  // in the upper triangular matrix are set to the sum of the row and column indices. The values in the lower
-  // triangular matrix are the negatives thereof.
-  auto weightfn = [](const std::array<int, 2>& coords) {
-
-    const auto [row, col] = coords;
-
-    // diagonal
-    if (row == col) {
-      return -1;
-    }
-    // upper triangular matrix
-    if (row < col) {
-      return row + col + 2;
-    }
-    // lower triangular matrix
-    return -(row + col + 2);
-  };
-
-  // As in the previous example we use a symmetric 7p stencil, but instead of defining it ourselves, we use a
-  // predefined variable template. See the header file for more pre-defined stencils.
-  const auto mat = matrixgen::adjmat({{3, 3, 1}}, matrixgen::STENCIL<7>, weightfn);
-
-  // Print the matrix. Convert to a dense matrix to make the output look cleaner.
-  std::cout << std::endl << Eigen::MatrixXd(mat) << std::endl;
+  /**
+   * Create the very same matrix from example 1 but use available presets for
+   * the stencil and the weightfunction. See 'matrixgen/presets.hpp' for other
+   * predefined stencils and weighfunctions.
+   */
+  const auto mat = matrixgen::adjmat({3, 3, 1}, matrixgen::STENCIL<7>, matrixgen::constweight(1));
+  std::cout << std::endl << mat << std::endl;
 }
